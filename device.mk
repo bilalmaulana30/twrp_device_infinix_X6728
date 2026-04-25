83e19b9342dedfd46824349c0739149b38ceb7a0
Andri K. <260222165+Andrikurn@users.noreply.github.com>
2026-04-25 14:59:12 +0700

    Try new decryption binaries.

--- device.mk
+++ device.mk
@@ -82,6 +82,14 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1
+
+# Keystore Hal
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # MTK plpath utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
@@ -95,3 +103,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+	
+# Additional configs
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1
